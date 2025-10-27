77072bc6571fbee4d73d935cfcfb33038f6fcdf3
Daniel Galvan <danieldg.1989@gmail.com>
2025-10-26 23:50:45 -0300

    created detail of a movie

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,7 +1,69 @@
+import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom';
+
+import api from '../../services/api';
+import './movies.css';
+
 function Movies(){
+    const { id } = useParams();
+    const [movieDetail, setMovieDetail] = useState({});
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        async function loadMovieDetail() {
+            await api.get(`/movie/${id}`, {
+                params: {
+                    api_key: "b3c2a8f74a0e861f1d0342cd52ca657e",
+                    language: "pt-BR"
+                }
+            })
+            .then((response) => {
+                if (response.data) {
+                setMovieDetail(response.data);
+                console.log(response.data);
+                } else {
+                    console.log('erro inesperado');
+                }
+                setLoading(false);
+            })
+            .catch(() => {
+                console.log('filme não encontrado');
+                setLoading(false);
+            });
+        }
+
+        loadMovieDetail();
+
+        return () => {
+            console.log('componente desmontado');
+        }
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="movie-detail">
+                <h2>Loading...</h2>
+            </div>
+        );
+    }
+
     return (
-        <div>
-            <h1>Página Movies</h1>;
+        <div className="movie-detail">
+            <h1>{movieDetail.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${movieDetail.backdrop_path}`} alt={movieDetail.title}></img>
+
+            <h3>Sinopse</h3>
+            <span>{movieDetail.overview}</span>
+            <strong> Avaliação: {movieDetail.vote_average} / 10</strong>
+
+            <div className="buttons">
+                <button>Salvar</button>
+                <button>
+                    <a href="a">
+                        Trailer
+                    </a>
+                </button>
+            </div>
         </div>
     )
 }
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 /*
     Base da URL: https://api.themoviedb.org/3/
-    URL da API: movie/now_playing?api_key=b3c2a8f74a0e861f1d0342cd52ca657e&language=pt-BR
+    URL da API GET ALL: movie/now_playing
+    URL da API GET Detail: movie/id
     KEY: b3c2a8f74a0e861f1d0342cd52ca657e
     language: pt-BR
 */
